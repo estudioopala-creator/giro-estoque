65edc44cabc7848a85747bd9e127fa3ececf6ccf
estudioopala <estudioopala@gmail.com>
2025-12-10 16:52:06 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,25 @@
-self.addEventListener('install', () => { console.log("SW instalado"); });
-self.addEventListener('fetch', (e) => {});
+
+const CACHE_NAME = 'giro-estoque-v12';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
